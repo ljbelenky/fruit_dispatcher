4da3c360f650ff46cd86d42d6bf17c5e00f60110
Land <ljbelenky@gmail.com>
2020-10-24 09:38:11 -0600

    adding dates

--- fruit.py
+++ fruit.py
@@ -5,21 +5,30 @@ import pandas as pd
 from flask import Flask, render_template, request, url_for
 import json
 
+n = 100
+
 fruit = ['apple','orange','banana','mango','pear']
-quality = ['poor','fair','average','good','excellent']
+quality = ['poor','fair','average','good','excellent', np.nan]
 condition = ['unripe','ripe','overripe','rotten']
 style = ['fancy','regular', np.nan]
 weight_ounces = (2, 10)
 packaging = [True, False, np.nan]
+start_date = pd.Series(pd.date_range('2020-01-01', '2020-12-31', n)).sample(n, replace = True)
+end_date = start_date + choice(pd.timedelta_range('1 day', periods = n), n)
 
-n = 100
+start_date = start_date.dt.strftime('%Y-%m-%d')
+end_date = end_date.dt.strftime('%Y-%m-%d')
+
+
 
 fruits = pd.DataFrame({'type':choice(fruit, n),
                         'quality': choice(quality, n),
                         'weight': randint(*weight_ounces, n),
                         'condition': choice(condition, n),
                         'style': choice(style, n),
-                        'packaging': choice(packaging, n)
+                        'packaging': choice(packaging, n),
+                        'start_date': start_date,
+                        'end_date': end_date
                         })
 
 
